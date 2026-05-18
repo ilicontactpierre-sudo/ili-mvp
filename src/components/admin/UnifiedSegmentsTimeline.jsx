27c397203c6dcfacfed78041ba4148e5f7920379
ilicontactpierre-sudo <ili.contact.pierre@gmail.com>
2026-05-18 18:48:21 +0200

    auto save

--- src/components/admin/UnifiedSegmentsTimeline.jsx
+++ src/components/admin/UnifiedSegmentsTimeline.jsx
@@ -311,6 +311,7 @@ function SegmentTimelineRow({
                 onChange={(e) => onEditChange(index, e.target.value)}
                 onBlur={() => onEditBlur(index)}
                 onKeyDown={(e) => onEditKeyDown(index, e)}
+                onMouseUp={(e) => e.stopPropagation()}
                 autoFocus
                 style={{
                   width: '100%',
