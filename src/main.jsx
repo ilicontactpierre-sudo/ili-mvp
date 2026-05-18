4f532514877740a883aa5a526c70e57ef981326b
ilicontactpierre-sudo <ili.contact.pierre@gmail.com>
2026-05-18 22:22:31 +0200

    auto save

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './styles/global.css'
 import App from './App.jsx'
+import './styles/vfx.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
