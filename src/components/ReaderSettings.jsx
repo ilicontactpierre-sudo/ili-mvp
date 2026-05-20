2c5bd0731f9df20b3a43bc329a47ab929ddb31d2
ilicontactpierre-sudo <ili.contact.pierre@gmail.com>
2026-05-21 00:52:28 +0200

    auto save

--- src/components/ReaderSettings.jsx
+++ src/components/ReaderSettings.jsx
@@ -146,7 +146,7 @@ function playSettingsClic() {
       document.body.style.backgroundImage = "url('https://www.transparenttextures.com/patterns/natural-paper.png')"
       document.body.style.backgroundAttachment = 'fixed'
     } else if (isSynthwave) {
-      root.style.setProperty('--color-bg', '#020117')
+      root.style.setProperty('--color-bg', '#000000')
       root.style.setProperty('--color-text-focus', '#fdc7f9')
       root.style.setProperty('--color-text-blur', 'rgb(49, 61, 171)')
       root.style.setProperty('--font-primary', "'VT323', 'Courier New', monospace")
