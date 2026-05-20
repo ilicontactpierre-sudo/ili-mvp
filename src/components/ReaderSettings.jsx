e429a08cec9a16eb8addfbb35a6163339b470fc4
ilicontactpierre-sudo <ili.contact.pierre@gmail.com>
2026-05-21 01:00:28 +0200

    auto save

--- src/components/ReaderSettings.jsx
+++ src/components/ReaderSettings.jsx
@@ -148,7 +148,7 @@ function playSettingsClic() {
     } else if (isSynthwave) {
       root.style.setProperty('--color-bg', '#000000')
       root.style.setProperty('--color-text-focus', '#ffffff')
-      root.style.setProperty('--color-text-blur', 'rgb(255, 167, 237)')
+      root.style.setProperty('--color-text-blur', 'rgb(246, 255, 167)')
       root.style.setProperty('--font-primary', "'VT323', 'Courier New', monospace")
       root.style.setProperty('--blur-amount', '2px')
   document.body.style.backgroundImage = `
