210bb9225fc1b4f967a135e119c93f6201ff2a41
ilicontactpierre-sudo <ili.contact.pierre@gmail.com>
2026-05-27 21:52:52 +0200

    auto save

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
 export default defineConfig({
   plugins: [react()],
   server: {
     fs: {
-      allow: ['.', '/chemin/vers/ta/BoomLibrary']
+      allow: ['.', '/Volumes/TM 14Go/KONTAKT Instruments/SAMPLE LIBRARY/SOUND DESIGN/BOOM LIBRARY']
     }
   }
 })
