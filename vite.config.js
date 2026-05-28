c8520937dd86811b811001d771050acfe475b6d6
ilicontactpierre-sudo <ili.contact.pierre@gmail.com>
2026-05-28 07:43:47 +0200

    auto save

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
 export default defineConfig({
   plugins: [react()],
   server: {
     fs: {
       allow: ['.', '/Volumes/TM 14Go/KONTAKT Instruments/SAMPLE LIBRARY/SOUND DESIGN/BOOM LIBRARY']
+    },
+    proxy: {
+      '/api/preview-sound': 'http://localhost:3001',
     }
   }
 })
