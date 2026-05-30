c3513bf2cdfc2dfb87ca7c1b00f27f6cf2c4cb02
ilicontactpierre-sudo <ili.contact.pierre@gmail.com>
2026-05-30 17:43:57 +0200

    auto save

--- src/components/StoryReader.jsx
+++ src/components/StoryReader.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useLayoutEffect, useRef, useState } from 'react'
 import './StoryReader.css'
 import { renderMarkdown } from '../utils/renderMarkdown'
+import { applyEmojiMode } from '../utils/emojiDict'
 import { getVfxClass } from './admin/constants'
 import hapticEngine from '../engine/HapticEngine'
 
