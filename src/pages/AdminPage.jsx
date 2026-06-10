43ef38d8ac7754852872ba889260efc1f6c57bc2
ilicontactpierre-sudo <ili.contact.pierre@gmail.com>
2026-06-11 00:06:43 +0200

    auto save

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -919,7 +919,6 @@ function AdminPage() {
 
   // Charger une histoire depuis StoryLoader
   const handleLoadStory = (storyData) => {
-  console.log('[loadStory] données reçues :', JSON.stringify(storyData, null, 2))
   // ...reste du code
     setStoryTitle(storyData.title || '')
     setStoryAuthor(storyData.author || '')
