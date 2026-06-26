3ed7978416847427f819d183333bac5a07c535c8
ilicontactpierre-sudo <ili.contact.pierre@gmail.com>
2026-06-26 18:57:32 +0200

    auto save

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import StoryMenu from '../components/StoryMenu';
 import ReaderSettings from '../components/ReaderSettings.jsx'
 import { playClicILi } from '../App.jsx'
