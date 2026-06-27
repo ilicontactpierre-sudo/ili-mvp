2b7a69df9042878f9bf4d588bfcbe369398940ef
ilicontactpierre-sudo <ili.contact.pierre@gmail.com>
2026-06-27 11:18:02 +0200

    auto save

--- src/components/admin/FormatToolbar.jsx
+++ src/components/admin/FormatToolbar.jsx
@@ -73,7 +73,7 @@ export default function FormatToolbar({ position, onFormat, onFontChange, curren
         style={{
           position: 'fixed',
           top,
-          left: 'min(calc(50% - 400px - 12px), calc(100vw - 60px))',
+          left: position.left ?? 'min(calc(50% - 400px - 12px), calc(100vw - 60px))',
           transform: 'translateX(-100%)',
           display: 'flex',
           flexDirection: 'column',
