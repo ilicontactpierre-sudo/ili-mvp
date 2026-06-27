8c50800bb48e66bb865ec9f06da86ea0f6292e59
ilicontactpierre-sudo <ili.contact.pierre@gmail.com>
2026-06-27 13:07:46 +0200

    auto save

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -2943,6 +2943,7 @@ function AdminPage() {
           position: 'fixed',
           left: timelineRect.right > 0 ? `${timelineRect.right + 10}px` : 'auto',
           right: timelineRect.right > 0 ? 'auto' : '8px',
+          display: isSplitView ? 'none' : 'flex',
           top: '50%',
           transform: 'translateY(-50%)',
           zIndex: 500,
