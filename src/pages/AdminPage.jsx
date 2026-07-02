47807c70d0e1bc7be2b3cd8d18112d21befc003e
ilicontactpierre-sudo <ili.contact.pierre@gmail.com>
2026-07-02 17:29:25 +0200

    auto save

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -2577,7 +2577,7 @@ function AdminPage() {
                       marginTop: '2rem', 
                       borderTop: '1px solid #eee', 
                       paddingTop: '1.5rem',
-                      scrollMarginTop: `${stickyHeight + 12}px`,
+                      scrollMarginTop: `${stickyHeight + 14}px`,
                     }}
                   >
                     {isMobile ? (
